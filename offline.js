5c3180e0dee63bd5671d8153352d6227657f3fca
semicon <30399464+semicon@users.noreply.github.com>
2019-01-05 13:46:41 +0700

    fanpantae

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1546667747,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/box-sheet0.png",
+		"images/picture-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/butn-sheet0.png",
+		"images/check-sheet0.png",
+		"images/check-sheet1.png",
+		"images/check-sheet2.png",
+		"images/replay-sheet0.png",
+		"images/close-sheet0.png",
+		"images/go-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"bird_list.txt"
+	]
+}
